ccfc4e9ca5e41445b83d0552fa0e8debfded4512
Sam Tobin-Hochstadt <samth@racket-lang.org>
2004-11-09 20:50:57 +0000

    fixed grade error

--- teaching/211-04/all-grades.scm
+++ teaching/211-04/all-grades.scm
@@ -1,36 +1,38 @@
+  (define jeff-grades ;; up to date 11-8
+'(;                        hw01 hw02   hw03   hw04   hw05        hw07       hw08
+ ("Rishi Aggarwal"        15   (19 1) (22 1) (29 0) (30     0)  (47     1) (29 1)) ; rishi@rjsnetworks.com
+ ("Jonathan Albertelly"   18   (0  1) (21 1) (30 1) (29     1)  (44     1) (29 1))
+ ("Andrew Alix"           19   (14 1) (13 1) (30 1) (32     0)  (27     0) (28 1)) ; alix.an@neu.edu
+ ("Thomas Blatchford"      6   (8  1) (23 1) (30 1) (21     1)  (19     1) (35 1)) ; tomb@ccs.neu.edu
+ ("Chris Chalifour"       19   (21 1) (32 1) (37 0) (31     0)  (47     0) (29 1)) ; cchali4@ccs.neu.edu
+ ("Adem Chich"            17   (18 1) (13 1) (29 0) (30     0)  (40     1) (24 1)) ; achich@ccs.neu.edu
+ ("Kristin Farris"        16   (0  1) (29 1) (33 0) (28     1)  (48     1) (36 1)) ; farris.k@neu.edu
+ ("Corey Gamache"         20   (0  1) (16 1) (24 1) (28     1)  (17     1) (31 1)) ; gamache.c@neu.edu
+ ("Thomas Harrold"        18   (18 1) (13 1) (29 1) (30     1)  (40     0) (24 1)) ; tharrold@ccs.neu.edu
+ ("Skyler Insler"         ?    (22 1) (0  1) (0  0) (00 false)  (0  false) (0  1))
+ ("Kari Karvinen"         18   (0  1) (16 1) (24 1) (28     0)  (17     1) (31 1)) ; kkarvine@ccs.neu.edu
+ ("Olga Klepikova"        19   (20 1) (23 1) (31 0) (26     0)  (44     1) (33 1)) ; olgak@ccs.neu.edu
+ ("Carol Lee"             16   (0  1) (29 1) (33 1) (28     1)  (48     1) (36 1))
+ ("Sang Hyun Lee"          8   (8  1) (23 1) (30 0) (21     1)  (19     0) (35 1)) ; lee.san@neu.edu
+ ("Christopher Looney"    14   (16 1) (0  1) (0  0) (00 false)  (0  false) (0  1))
+ ("Daniel Marcucci"       19   (12 1) (17 1) (34 1) (31     1)  (30     1) (36 1))
+ ("Jeremy Mckenzie"       ?    (0  1) (21 1) (30 0) (29     0)  (44 false) (0  1)) ; aquila@ccs.neu.edu
+ ("Mike Nelligan"         16   (7  1) (17 1) (0  0) (27     1)  (7      1) (24 1)) ; nelligan.mic@neu.edu
+ ("Daniel Noonan"         16   (19 1) (22 1) (29 0) (30     0)  (34     1) (34 1)) ; djn@ccs.neu.edu
+ ("Thomas Pappas"         ?    (20 1) (23 1) (31 1) (26     1)  (44     1) (33 1))
+ ("Daniel Rabinowitz"     17   (0  1) (19 1) (34 1) (30     0)  (40     1) (33 1)) ; drabino9@ccs.neu.edu
+ ("Ross Ramsay"           17   (14 1) (13 1) (30 1) (32     1)  (27     1) (28 1))
+ ("Eileen Ryan"           20   (16 1) (0  1) (0  0) (31     0)  (30     1) (36 1)) ; ryan.ei@neu.edu
+ ("Mark Shoaie"           15   (16 1) (27 1) (0  1) (29     0)  (47     1) (31 1)) ; shoaiem@ccs.neu.edu
+ ("Matthew Skolnick"      17   (12 1) (17 1) (34 0) (00 false)  (0  false) (0  1))
+ ("Manolo Solis"          17   (21 1) (32 1) (37 1) (00 false)  (0  false) (0  1))
+ ("Jon Sporn"             ?    (16 1) (27 1) (0  1) (29     1)  (47     0) (31 1)) ; ssporn@ccs.neu.edu
+ ("Brian Tobia"           ?    (0  1) (19 1) (34 1) (30     1)  (40 false) (33 1))
+ ("Andres Weber"           8   (7  1) (17 1) (0  1) (27 false)  (7      0) (24 1)) ; fayth@ccs.neu.edu
+ ("Chris Willis"          18   (19 1) (22 1) (29 0) (30 false)  (34     1) (34 1)) ; chrismw@ccs.neu.edu
 
-  (define jeff-grades ;; outdated
-'(("Rishi Aggarwal" 15 (19 1) (22 1) (19 1) (30 0))
- ("Jonathan Albertelly" 18 (0 1) (21 1) (30 1) (29 1))
- ("Andrew Alix" 18 (14 1) (13 1) (30 1) (32 1))
- ("Thomas Blatchford" 6 (8 1) (23 1) (30 1) (21 1))
- ("Chris Chalifour" 19 (21 1) (32 1) (37 0) (0 1))
- ("Adem Chich" 17 (18 1) (13 1) (29 0) (30 0))
- ("Kristin Farris" 16 (0 1) (29 1) (33 0) (28 1))
- ("Corey Gamache" 20 (0 1) (16 1) (24 1) (28 1))
- ("Thomas Harrold" 18 (18 1) (13 1) (29 1) (30 1))
- ("Skyler Insler" ? (22 1) (0 1) (0 0) (0 1))
- ("Kari Karvinen" 18 (0 1) (16 1) (24 1) (28 0))
- ("Olga Klepikova" 19 (20 1) (23 1) (31 1) (26 0))
- ("Carol Lee" 16 (0 1) (29 1) (33 1) (28 1))
- ("Sang Hyun Lee" 8 (8 1) (23 1) (30 0) (21 1))
- ("Christopher Looney" 14 (16 1) (0 1) (0 0) (0 1))
- ("Daniel Marcucci" 19 (12 1) (17 1) (34 1) (31 1))
- ("Jeremy Mckenzie" ? (0 1) (21 1) (30 1) (29 0))
- ("Mike Nelligan" 16 (7 1) (17 1) (0 0) (27 1))
- ("Daniel Noonan" 16 (19 1) (22 1) (19 1) (30 0))
- ("Thomas Pappas" ? (20 1) (23 1) (31 1) (26 1))
- ("Daniel Rabinowitz" 17 (0 1) (19 1) (34 1) (30 1))
- ("Ross Ramsay" 17 (14 1) (13 1) (30 1) (32 1))
- ("Eileen Ryan" 20 (16 1) (0 1) (0 1) (31 1))
- ("Mark Shoaie" 15 (0 1) (27 1) (0 1) (29 0))
- ("Matthew Skolnick" 17 (12 1) (17 1) (34 0) (0 1))
- ("Manolo Solis" 17 (21 1) (32 1) (37 1) (0 1))
- ("Jon Sporn" ? (0 1) (27 1) (0 1) (29 1))
- ("Brian Tobia" ? (0 1) (19 1) (34 1) (30 1))
- ("Andres Weber" 8 (7 1) (17 1) (0 1) (27 1))
- ("Chris Willis" 18 (19 1) (22 1) (19 1) (30 0))))
 
+))
 
 
   (define sam-grades ;; up to date 11-5
@@ -45,14 +47,14 @@
       ("Wesley Werbeck"            18 (12 1) (20 1) (25 1) (28 0) (29 1) 29)
       ("Bassil Silver-Hajo"        16 (15 1) (#f 1) (13 1) (25 1) (41 1) 21)
       ("Eric Blumenstyk"           13 (15 1) (#f 1) (13 1) (25 1) (41 1) 21)
-      ("Carmine Silano"            20 (11 1) (25 1) (21 1) (29 0) (38 1) 18)
-      ("Suraj Laungani"            #f (11 1) (25 1) (21 0) (29 1) (38 1) 18)
+      ("Carmine Silano"            20 (11 1) (25 1) (21 1) (29 0) (38 1) 19)
+      ("Suraj Laungani"            #f (11 1) (25 1) (21 0) (29 1) (38 1) 19)
       ("Chris Masciarelli"         17 (17 1) (3 1)  (11 0) (9 0)  (4 0)  2)
       ("Matt Haslett"              #f (12 0) (3 1)  (11 1) (9 0)  (4 0)  2)
       ("Christos Goumenos"         19 (13 1) (26 1) (26 1) (24 1) (36 1) 37)
       ("Marc Mestre"               15 (13 1) (26 1) (26 1) (24 0) (36 1) 37)
       ("Dan Schiffman"             #f (#f 1) (13 1) (8 1)  (26 0) (42 1) 26) 
-      ("Jerry Mui"                 12 (13 1) (#f 1) (8 1)  (26 0) (42 0) 26)
+      ("Jerry Mui"                 12 (13 1) (10 1) (8 1)  (26 0) (42 0) 26)
       ("Elvin Taullaj"             #f (16 1) (27 1) (34 0) (28 0) (43 #f)25)
       ("Kenneth Eimer"             18 (16 1) (27 1) (34 1) (28 1) (43 1) 25)
       ("Gregg Hurson"              1 (7 1)  (8 1) (20 1) (20 0) (32 0) 23)
