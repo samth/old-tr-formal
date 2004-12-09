e49e5b318b5bb0b7e38c138ae859386a3c7c26fb
Sam Tobin-Hochstadt <samth@racket-lang.org>
2004-12-09 03:56:29 +0000

    it works- whee

--- metaclasses/fool-present.scm
+++ metaclasses/fool-present.scm
@@ -2,9 +2,33 @@
 
   (require (prefix : (lib "symbol.ss" "texpict")))
   (require "jcode.scm")
+  
+  (define (jcode/l . args) (page-para (apply jcode args)))
 
   (define (small t) (text t (current-main-font) (* 2 (quotient (current-font-size) 3))))
   (define (sub t) (text t (cons 'subscript (current-main-font)) (current-font-size)))
+  
+  (define (many-subitems . bullets)
+    (let ((items (map car bullets))
+          (subitems (map cadr bullets)))
+      (let loop ((si (car subitems))
+                 (other-si (cdr subitems))
+                 (past-it '())
+                 (it (car items))
+                 (other-it (cdr items)))
+        (cons
+         (append
+          (map page-item past-it)
+          (list* (page-item it)
+                 (page-subitem si)
+                 (map page-item other-it)))
+         (if (null? other-si) '()
+             (loop (car other-si) 
+                   (cdr other-si)
+                   (append past-it (list it))
+                   (car other-it)
+                   (cdr other-it)))
+        ))))
   
   (slide/center
    (titlet "A Core Calculus of Metaclasses")
@@ -41,7 +65,7 @@
   (slide/title "Why Metaclasses?"
                (page-item "Modeling the World")
                (page-item "Flexible Hierarchies")
-               (page-item "Generalized Statics")
+               (page-item "Generalized Static Methods")
                ;(page-item "Design Patterns")
                )
 
@@ -167,7 +191,7 @@
                'next
                
                (page-item "Every class has a metaclass (also referred to as " 
-                          (it "kind" )")")
+                          (it "kind" )"), and is an instance of its metaclass.")
 ;               (blank)
                (jcode "class Eagle kind Species")
 
@@ -177,9 +201,59 @@
                
                )
   
+  (define (page-subitem/color first color . rest)
+    (apply page-subitem first (map (lambda (x) (if (string? x)
+                                                   (colorize (t x) color)
+                                                   (colorize x color))) rest)))
+  
+  (slide/title "A Simple Example" ; FIXME - REFORMAT!!
+               'alts
+               (let ((class-c (jcode "class C {"
+                                 "    int x;"
+                                 "}"))
+                     (myclass (jcode "class MyClass kind C {"
+                                     "    class int x = 5;"
+                                     "    int m(int y) { return y+2; }"
+                                     "}") )) 
+                 (list 
+                  (list (vl-append class-c (ghost myclass)))
+                  (list (vl-append class-c  myclass))))
+               'next
+               (page-para "All of the following are valid expressions:")
+               (page-subitem (jcode "C"))
+               (page-subitem (jcode "MyClass.x"))
+               (page-subitem/color (jcode "mc.m(5)") "blue" "   if " (jcode "mc") 
+                            "is an instance of" (jcode "MyClass"))
+               (page-subitem/color  (jcode "c.x") "blue" "  if " (jcode "c") "is an instance of "
+                            (jcode "C"))
+               )
+  
+  (slide/title "Inheritance"
+               (page-para "As in many other language, classes inherit instance"
+                          "members from their superclass.")
+               (blank)
+               'next
+               (page-para "But classes also inherit class members from their metaclasses."
+                          )
+               (page-para "Since classes are instances of their kind, they must inherit the"
+                          (it "instance") "members of their kind as" (it "class") 
+                          "members.")
+               'next
+               (page-para "For example, if" (jcode "C") "has kind" (jcode "K") "with the following definitions:")
+               (page-para/c (hc-append
+                             (jcode "class K {"
+                                    "    class int m() { ... }"
+                                    "}")
+                             (jcode "class C kind K {"
+                                    "    class int n() { ... }"
+                                    "}")))
+               (blank)
+               (page-para "Then" (jcode "C") "has two class methods, m and n.")
+               )
+  
   (slide/title "Object Creation with Constructors"
                (page-para "To create an instance of class C, we call the constructor as follows:")
-               (jcode "new(e1,e2)")
+               (jcode "new(e1)")
                (page-para "which initializes all the fields of class C in order"
                           "and produces an instance of C.")
                'next
@@ -191,9 +265,56 @@
                (page-para "In these examples, we didn't specify which class.  We'll see soon why that isn't neccessary.")
                )
   
-  (slide/title "Object Creation with Classes")
+  (slide/title "Object Creation with Classes"
+               (page-para "Since classes are instances of their kinds, declaring a class"
+                          "also creates an instance.")
+               (page-para "So how do we initialize fields?")
+               'next
+               (page-para "We have to give values to all " (jcode "class") "fields,"
+                          "including those inherited from the kind.")
+               'next
+               (jcode "class MyClass kind C {"
+                                 "    class int x = 5;"
+                                 "    int m(int y) { y+2; }"
+                                 "}")
+               (page-para "We initialize the inherited class field " (jcode "x"))
+               )
   
-  (slide/title "Method Dispatch")
+  (slide/title "Method Dispatch"
+               (page-para "Where do we look to find the method to call?")
+               (blank)
+               'alts
+               (list 
+                (list
+                 (page-para "For invoking methods on" 
+                            (colorize (t "objects created by constructors") "green") ":")
+                 (page-item/bullet (colorize (t "1") "blue")
+                                   "Look in the definition of the class for which " 
+                                   "this" (colorize (t"object") "green") "is an instance.")
+                 (page-item/bullet (colorize (t "2") "blue")
+                                   "Look in the superclass of the class you're looking in.")
+                 (page-item/bullet (colorize (t "3") "blue")
+                                   "Repeat step 2."))
+                (list
+                 (page-para "For invoking methods on" 
+                            (colorize (t "instance classes") "green") ":")
+                 (colorize
+                  (page-item/bullet (colorize (t "0") "blue")
+                                    "Look in the definition of the class.") "green")
+                 (page-item/bullet (colorize (t "1") "blue")
+                                   "Look in the definition of the class for which " 
+                                   "this" (colorize (t"class") "green")"is an instance.")
+                 (page-item/bullet (colorize (t "2") "blue")
+                                   "Look in the superclass of the class you're looking in.")
+                 (page-item/bullet (colorize (t "3") "blue")
+                                   "Repeat step 2.")))
+               'next
+               (page-para "Since classes" (it "are") "objects, can these definitions can"
+                          "be collapsed?")
+               'next
+               (page-para (bt "No.")  "Step 0 above has no analogue for"
+                          "instances that are not classes.")
+               )
   
   (outline 'benefit)
   
@@ -218,20 +339,119 @@
                'next
                (page-para "So, every constructor is private to the class that it constructs.  Any other creation of objects must go through a factory method."))
   
-  (slide/title "Design Patterns in the Language")
+  (slide/title "Design Patterns in the Language"
+               (page-para "Factory Pattern")
+               (page-subitem "We just saw it.")
+               'next
+               (page-para "Singleton Pattern :")
+               (jcode "class MySingleton kind C { ... }")
+               'next 
+               (page-para "Prototype Pattern") 
+               )
   
-  (slide/title "Singleton Pattern")
-  
-  (slide/title "Factory Pattern")
+
   
   (outline 'details)
   
   (slide/title "typeOf"
-               (page-para "What is the type of the expression" (jcode "C") "?"))
+               (page-para "What is the type of the expression" (jcode "C") "?")
+               'next
+               (page-para "Can we just use the kind of C?")
+               'next
+               (page-para "No.")
+               (vl-append
+                (jcode "class D {}"
+                       " "
+                       "class C kind D {"
+                       "    class int m() { ... }"
+                       "}"))
+               (blank)
+               (page-para "Now" (jcode "C.m()") "will fail to typecheck.")
+               'next
+               (page-para "We need some way of referring to just the type of" (jcode "C"))
+               (page-para "We refer to this type as" (jcode "typeOf[C]")))
   
   (slide/title "Generics and Metaclasses"
                (page-para "Do metaclasses have implications for the use of "
-                          "parametric polymorphism?"))
+                          "parametric polymorphism?")
+               (page-para "If T is a type variable, how do we typecheck")
+               (jcode "T.m()")
+               'next
+               (page-para "We need to add bounds on the kind of type variables."))
+  
+
+  (slide/title "Related Work"
+               'alts
+               (many-subitems 
+                (list "ObjVLisp"
+                      "Very similiar model, without types")
+                (list "Smalltalk"
+                      "Restricted levels")
+                (list "MOP"
+                      "???")
+                (list "Python"
+                      "Similar to ObjVLisp, no published semantics")
+                (list "SOL?"
+                      "IBM Language from 1990's")
+                (list "OOPSLA 2004"
+                      "Insipration for this work, but no semantics")))
+               
+  #;(slide/title "Related Work"
+               (page-item "ObjVLisp")
+               (page-subitem "Very similiar model, without types")
+               (page-item "Smalltalk")
+               (page-subitem "Restricted levels")
+               (page-item "MOP")
+               (page-subitem "???")
+               (page-item "Python")
+               (page-subitem "Similar to ObjVLisp, no semantics given")
+               (page-item "SOL?") ;; FIXME
+               (page-subitem "IBM Language from early 90's")
+               (page-item "OOPSLA 2004") ;; FIXME
+               (page-subitem "Inspiration for this work, but not a semantics")
+               ) 
+  
+  (slide/title "Formalism"
+               (page-para "A simple extension of Featherweight GJ")
+               (blank)
+               (page-item "The only significant increase in complexity in the formalism results from classes as expressions.")
+               'next
+               (page-subitem "Which is even present in Java!")
+               'next
+               (page-item "The proof of soundness is not much more complex than for FGJ.")
+               'next
+               (blank)
+               (page-para "Metaclasses are not that complicated!"))
+  
+  (slide/title "Problems and Future Work"
+               (page-item "Adding mutable state would require some work. ")
+               (page-item "Our constructor reform does badly for initializing superclass fields.")
+               (page-item "Implementation"))
+  
+  (slide/title "Conclusions"
+               (page-item "We've shown why metaclasses are benefical for an OO language.")
+               (page-item "We have presented a way to extend a language with metaclasses.")
+               (page-item "We have discovered that metaclasses can make constructors much simpler.")
+               (page-item "We formalized our calculus, and it wasn't that bad.")
+               )
   
   (slide/center
-   (bt "Thank You")))
+   (bt "Thank You"))
+  
+  (slide/center
+   (t "Auxilliary Slides"))
+  
+  (slide/title "Species in Java"
+               (jcode/l "class Species { ... }")
+               (jcode/l "Species theEagle = new Species()")
+               (jcode/l "Species theSalmon = new Species()")
+               (jcode/l "class Eagle { ... }")
+               (jcode/l "Eagle harry = new Eagle()")
+               (blank)
+               'next
+               'alts
+               (list
+                (list (page-para "How many invariants are there in this code?"))
+                (list (page-para "How many invariants are there in this code : 4"))) ; FIXME
+               ) 
+  )
--- test.scm
+++ test.scm
@@ -2,16 +2,25 @@
   
   (require (lib "match.ss") (lib "list.ss") (lib "etc.ss") (lib "contract.ss"))
   
+  (require "useful.scm")
+  
+  (define current-expr (make-parameter #f))
+  
   (define (simplify-list l)
     (let loop ((l l) (result l))
       (if (null? l) result
           (loop (cdr l)
                 (cons (car l)
                       (filter (lambda (x) (not (equal? x (car l))))
-                        result))))))
+                              result))))))
   
   (define (extend env key val)
     (cons (list key val) env))
+  
+  (define (extends env . key-vals)
+    (foldr (lambda (x y) (extend y (car x) (cadr x)))
+           env key-vals))
+  
   
   (define-syntax define*/c
     (syntax-rules ()
@@ -34,78 +43,113 @@
   
   (define type/c any/c)
   
-  ; alts is a list of types
-  (define-struct union-ty (alts) (make-inspector))
-  
-  (define (mk-union-ty l)
+    (define (mk-union-ty l)
     (let ((l (simplify-list l)))
-          (if (= 1 (length l)) (car l) (make-union-ty l))))
+      (if (= 1 (length l)) (car l) (make-union-ty l))))
   
   (print-struct #t)
   
-  ; name is a symbol
-  (define-struct ty-ref (name) (make-inspector))
-
+  
+  
+  
+  (define-syntax type 
+    (syntax-rules (-> : List)
+      [(_ nm : args ... -> ret)
+       (list (quote nm)
+             (make-fun-ty (list (type args) ...)
+                          (type ret)))]
+      [(_ nm : t) (list (quote nm) (type t))]
+      [(_ nm : List t) (make-list-ty (type t))]
+      [(_ t) (quote t)]))
+  
+  (define-syntax types
+    (syntax-rules ( : )
+      [(_ (a . b) ...)
+       (list (type a . b) ...)]))
+  
+  (define base-env
+    (types 
+     (+ : Number Number -> Number)
+     (- : Number Number -> Number)
+     (* : Number Number -> Number)
+     (/ : Number Number -> Number)
+     (and : Boolean Boolean -> Boolean)
+     (foo : Number)
+     (equal? : Any Any -> Boolean)
+     (< : Number Number -> Boolean)
+     (> : Number Number -> Boolean)
+     (= : Number Number -> Boolean)
+     (not : Boolean -> Boolean)
+     (number? : Any -> Boolean)
+     (boolean? : Any -> Boolean)
+     (symbol? : Any -> Boolean)
+     (cons? : Any -> Boolean)
+     (empty? : Any -> Boolean)
+     (argmax : Number Number -> Number)
+     ))
+  
   (define* lon (mk-union-ty `(Empty (Number . ,(make-ty-ref 'LoN)))))
   
   (define* nelon (mk-union-ty `((Number . Empty) (Number . ,(make-ty-ref 'nelon)))))
   
   ; alist of symbol * type 
   (define* tyenv `((LoN ,lon)
-                  (nelon ,nelon)))
-
+                   (nelon ,nelon)))
+  
   ; sym alist[sym*x] -> x 
-  (define*/c (lookup sym env)
-    (symbol?  (listof (list/c symbol? any/c)). -> . any/c)
-    (and (assoc sym env) (cadr (assoc sym env))))
+  (define* (lookup sym env)
+           (cond [(assoc sym env) => cadr]
+                 [else (printf "failed to find symbol ~a~n" sym) #f]))
   
   (define (id x) x)
   
   ; (x -> y or #f) listof[x] -> listof[y]
   (define*/c (map-opt f l)
-    ((any/c . -> . any/c) (listof any/c) . -> . (listof any/c))
-    (filter id (map f l)))
+             ((any/c . -> . any/c) (listof any/c) . -> . (listof any/c))
+             (filter id (map f l)))
   
   ; type -> type
   ; unfold a recursive list type
   (define*/c (cons?-trans ty) 
-    (-> any/c any/c)
-    (match ty
-      [($ ty-ref t) (cons?-trans (lookup t tyenv))]
-      [(_ . _) ty]
-      [($ union-ty (cl ...))
-       (mk-union-ty (map-opt cons?-trans cl))]
-      [ty #f])
-    )
+             (-> any/c any/c)
+             (match ty
+               [($ ty-ref t) (cons?-trans (lookup t tyenv))]
+               [(_ . _) ty]
+               [($ union-ty (cl ...))
+                (mk-union-ty (map-opt cons?-trans cl))]
+               [ty #f])
+             )
   
   ; type -> type
   (define*/c (empty?-trans ty) 
-    (-> type/c type/c)
-    (match ty
-      [($ ty-ref t) (empty?-trans (lookup t tyenv))]
-      ['Empty ty]
-      [($ union-ty (cl ...))
-       (mk-union-ty (map-opt empty?-trans cl))]
-      [ty #f])
-    )
+             (-> type/c type/c)
+             (match ty
+               [($ ty-ref t) (empty?-trans (lookup t tyenv))]
+               ['Empty ty]
+               [($ union-ty (cl ...))
+                (mk-union-ty (map-opt empty?-trans cl))]
+               [ty #f])
+             )
   
- ; type (type -> type) -> type 
+  ; type (type -> type) -> type 
   (define*/c (update-type-car ty transform)
-    (type/c (type/c . -> . type/c) . -> . type/c)
-    (match ty
-      [($ ty-ref t) (update-type-cdr (lookup t tyenv) transform)]
-      [($ union-ty (cl ...)) (mk-union-ty (map-opt (lambda (x) (update-type-car x transform)) cl))]
-      [_ (let ((t (transform (car ty))))
-           (and t (cons  t (cdr ty)) ))]))
-
+             (type/c (type/c . -> . type/c) . -> . type/c)
+             (match ty
+               [($ ty-ref t) (update-type-cdr (lookup t tyenv) transform)]
+               [($ union-ty (cl ...)) 
+                (mk-union-ty (map-opt (lambda (x) (update-type-car x transform)) cl))]
+               [_ (let ((t (transform (car ty))))
+                    (and t (cons t (cdr ty)) ))]))
+  
   ; type (type -> type) -> type  
   (define*/c (update-type-cdr ty transform)
-    (type/c (type/c . -> . type/c) . -> . type/c)
-    (match ty
-      [($ union-ty (cl ...)) (mk-union-ty (map-opt (lambda (x) (update-type-cdr x transform)) cl))]
-      [($ ty-ref t) (update-type-cdr (lookup t tyenv) transform)]
-      [_ (let ((t (transform (cdr ty))))
-           (and t (cons (car ty) t)))]))
+             (type/c (type/c . -> . type/c) . -> . type/c)
+             (match ty
+               [($ union-ty (cl ...)) 
+                (mk-union-ty (map-opt (lambda (x) (update-type-cdr x transform)) cl))]
+               [($ ty-ref t) (update-type-cdr (lookup t tyenv) transform)]
+               [_ (let ((t (transform (cdr ty))))
+                    (and t (cons (car ty) t)))]))
   
   (define (update-type exp env transform)
     (match exp
@@ -126,23 +170,40 @@
       [('cons? e) (update-type e env cons?-trans)]
       [('empty? e) (update-type e env empty?-trans)]))
   
-  (define equal-ty 
-    (match-lambda*
-      [('Any _) #t]
-      [(_ 'Any) #t] 
-      [(t (and g ($ union-ty ts)))
-       (andmap (lambda (x) (equal? t x)) ts)]
-      [(goal actual)
-       (equal? goal actual)]))
+  (define (print-env env)
+    (printf "Environment:~n")
+    (for-each (lambda (x) (printf "~a~n" x)) env)
+    (newline))
+  
+  (define env1 `((nl ,(make-ty-ref 'nelon))))
+  
+  (define back-nelon (make-union-ty `((Number . Empty) (Number . ,(make-ty-ref 'nelon)))))
+  
+  (define (equal-ty a b)
+    (if (equal? a b) #t
+        (match (list a b)
+          [('Any _) #t]
+          [(_ 'Any) #t] 
+          [(($ ty-ref t) b) (equal-ty (lookup t tyenv) b)]
+          [(a ($ ty-ref t)) (equal-ty (lookup t tyenv) a)]
+          [(($ union-ty alts1) ($ union-ty alts2))
+           (printf "comparing unions~n~a~n~a~n" a b)
+           (andmap equal-ty (quicksort alts1 compare) (quicksort alts2 compare))]
+          [(($ union-ty ts) t)
+           (andmap (lambda (x) (equal? t x)) ts)]
+          [(t ($ union-ty ts))
+           (andmap (lambda (x) (equal? t x)) ts)]
+          [_ #f])))
   
   (define (check-equal-ty goal actual)
     (if (not (equal-ty goal actual))
         (type-error goal actual)))
-       
-           
-
+  
+  
+  
   (define (type-error goal actual)
-    (printf "typecheck failed: ~a and ~a~n" goal actual))
+    (printf "typecheck failed: ~a and ~a~n" goal actual)
+    (printf "while checking: ~a~n" (current-expr)))
   
   (define (car-of-ty t)
     (match t
@@ -162,44 +223,70 @@
   
   (define (check-cond-clause clause goal env)
     (match clause
-      [('else _) (error "else not supported")]
-      [(question answer)
+      [('else answer) (check-exp answer goal env)]
+      [((and question (or ('cons? _) ('empty? _))) answer)
+       (printf "checking a cons?: ~a~n" question)
+       (check-exp question 'Boolean env)
        (let ((new-env (extend-env-from-q question env)))
-         (check-exp answer goal new-env))]))
+         (check-exp answer goal new-env))]
+      [(question answer)
+       (check-exp question 'Boolean env)
+       (check-exp answer goal env)]))
   
   (define (check-cond clauses goal env)
     (for-each (lambda (cl) (check-cond-clause cl goal env)) clauses) 
     goal)
   
   (define (check-exp expr goal env)
-    (match expr
-      [('cond clauses ...) (check-cond clauses goal env)]
-      [(or 'empty '()) (check-equal-ty goal 'Empty) goal]
-      [(? number? e) (check-equal-ty goal 'Number) goal]
-      [('car e) (let* ((t (check-exp e 'Any env))
-                       (car-t (car-of-ty t)))
-                      (check-equal-ty goal car-t)
-                  car-t)]
-      [('cdr e) 
-;       (printf "env: ~a" env)
-       (let* ((t (check-exp e 'Any env))
-              (cdr-t (cdr-of-ty t)))
-         (check-equal-ty goal cdr-t)
-         cdr-t)]
-      [(? symbol? exp) 
-       (newline)
-       (display env)
-       (let ((var-ty (lookup exp env)))
-         (check-equal-ty goal var-ty)
-         var-ty)]
-      ))
+      (match expr
+        [('cond clauses ...) (check-cond clauses goal env)]
+        [(or 'empty '()) (check-equal-ty goal 'Empty) goal]
+        [(? number? e) (check-equal-ty goal 'Number) goal]
+        [('car e) (let* ((t (check-exp e 'Any env))
+                         (car-t (car-of-ty t)))
+                    (check-equal-ty goal car-t)
+                    car-t)]
+        [('cdr e) 
+         ;       (printf "env: ~a" env)
+         (let* ((t (check-exp e 'Any env))
+                (cdr-t (cdr-of-ty t)))
+           (check-equal-ty goal cdr-t)
+           cdr-t)]
+        [(or 'true 'false) 'Boolean]
+        [(? symbol? exp) 
+         ;(newline)
+         ;(display env)
+         (let ((var-ty (lookup exp env)))
+           (check-equal-ty goal var-ty)
+           var-ty)]
+        [((? symbol? func) . args)
+         ;(print-env env)
+         (parameterize ((current-expr (cons func args)))
+           (let* ((func-type (lookup func env))
+                  (func-args (fun-ty-args func-type))
+                  (func-ret (fun-ty-result func-type)))
+             (for-each (lambda (arg ty) (check-exp arg ty env))
+                       args func-args)
+             (check-equal-ty goal func-ret)
+             func-ret))]
+        
+        ))
+  
+  (define (zip a b)
+    (map list a b))
+  
+  #;  (match '(def (foo ((a b) (a b))) foo foo3) 
+        [('def (bar ((x y) ...)) baz baz2) 
+         (list baz2 bar baz x y)])
   
   (define (check-def def env)
     (match def
-      [('define (fun (and args ((arg ty) ...))) result
+      [('define (fun ((arg ty) ...)) result
          body)
-       (let ((new-env (append args env)))
-         (display new-env)
+       (let* ((func-ty (make-fun-ty ty result))
+              (new-env (apply extends (extend env fun func-ty) (zip arg ty))))
+         ;(display new-env)
+         ;(newline)
          (check-exp body result new-env))]))
   
   (define simple-def `(define (foo ((ll ,(make-ty-ref 'nelon)))) Number
@@ -208,9 +295,31 @@
   
   (define wrong-def `(define (foo ((ll ,(make-ty-ref 'nelon)))) Number
                        (cond [(empty? ll) 1]
-                             [(cons? ll) (car (cdr ll))])))
+                             [(cons? ll) (+ 1 (car (cdr ll)))])))
   (define simple-env 
     `((l ,(make-ty-ref 'LoN))
       (l2 ,(make-ty-ref 'nelon))))
+  
+  (define (argmax a b)
+    (cond [(> a b) a]
+          [else b]))
+  
+  (define test-argmax 
+    `(define (argmax ((a Number) (b Number))) Number
+       (cond [(> a b) a]
+             [else b])))
+  
+  (define test-max
+    `(define (my-max ((nl ,(make-ty-ref 'nelon)))) Number
+       (cond
+         [(empty? (cdr nl)) (car nl)]
+         [(cons? (cdr nl)) (argmax (car nl) (my-max (cdr nl)))])))
+  
+  (define (my-max nl)
+    (cond
+      [(empty? (cdr nl)) (car nl)]
+      [(cons? (cdr nl)) (argmax (car nl) (my-max (cdr nl)))]))
+  
+  (define ENV (append simple-env base-env))
   
   )
